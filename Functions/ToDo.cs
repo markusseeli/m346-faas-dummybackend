3d1f471edfdfc010b34d8951210b2d4ae68953b4
Markus Seeli <markus.seeli@sbl.ch>
2022-09-03 15:59:09 +0200

    Correction of initial Checkin

--- Functions/ToDo.cs
+++ Functions/ToDo.cs
@@ -1,21 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace ServerlessFuncs
+namespace Functions
 {
-    public class Todo
+    public class ToDo
     {
         public string Id { get; set; } = Guid.NewGuid().ToString("n");
         public DateTime CreatedTime { get; set; } = DateTime.UtcNow;
-        public string TaskDescription { get; set; }
+        public string TaskDescription { get; set; } = "New Todo";
         public bool IsCompleted { get; set; }
     }
 
-    public class TodoCreateModel
+    public class ToDoCreateModel
     {
         public string TaskDescription { get; set; }
     }
 
-    public class TodoUpdateModel
+    public class ToDoUpdateModel
     {
         public string TaskDescription { get; set; }
         public bool IsCompleted { get; set; }
--- Functions/ToDoApi.cs
+++ Functions/ToDoApi.cs
@@ -1,45 +1,47 @@
+using System;
 using System.IO;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using System.Threading.Tasks;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace ServerlessFuncs
+namespace Functions
 {
-    public static class TodoApi
+    public static class ToDoApi
     {
-        static List<Todo> items = new List<Todo>();
+        static List<ToDo> items = new List<ToDo>();
 
-        [FunctionName("CreateTodo")]
-        public static async Task<IActionResult> CreateTodo(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "todo")]HttpRequest req, TraceWriter log)
+        [FunctionName("CreateToDo")]
+        public static async Task<IActionResult> CreateToDo(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "todo")] HttpRequest req,
+            ILogger log)
         {
-            log.Info("Creating a new todo list item");
+            log.LogInformation("Creating a new todo list item");
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            var input = JsonConvert.DeserializeObject<TodoCreateModel>(requestBody);
+            var input = JsonConvert.DeserializeObject<ToDoCreateModel>(requestBody);
 
-            var todo = new Todo() { TaskDescription = input.TaskDescription };
+            var todo = new ToDo() { TaskDescription = input.TaskDescription };
             items.Add(todo);
             return new OkObjectResult(todo);
         }
 
-        [FunctionName("GetTodos")]
-        public static IActionResult GetTodos(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "todo")]HttpRequest req, TraceWriter log)
+        [FunctionName("GetToDos")]
+        public static IActionResult GetToDos(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "todo")] HttpRequest req, ILogger log)
         {
-            log.Info("Getting todo list items");
+            log.LogInformation("Getting todo list items");
             return new OkObjectResult(items);
         }
 
         [FunctionName("GetTodoById")]
         public static IActionResult GetTodoById(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "todo/{id}")]HttpRequest req, 
-            TraceWriter log, string id)
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "todo/{id}")] HttpRequest req,
+            ILogger log, string id)
         {
             var todo = items.FirstOrDefault(t => t.Id == id);
             if (todo == null)
@@ -51,17 +53,20 @@ namespace ServerlessFuncs
 
         [FunctionName("UpdateTodo")]
         public static async Task<IActionResult> UpdateTodo(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "todo/{id}")]HttpRequest req,
-            TraceWriter log, string id)
+            [HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "todo/{id}")] HttpRequest req,
+            ILogger log, string id)
         {
+            log.LogInformation($"Updating todo with the id {id}");
+
             var todo = items.FirstOrDefault(t => t.Id == id);
             if (todo == null)
             {
+                log.LogInformation($"Could not find a todo with the id {id} to update");
                 return new NotFoundResult();
             }
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            var updated = JsonConvert.DeserializeObject<TodoUpdateModel>(requestBody);
+            var updated = JsonConvert.DeserializeObject<ToDoUpdateModel>(requestBody);
 
             todo.IsCompleted = updated.IsCompleted;
             if (!string.IsNullOrEmpty(updated.TaskDescription))
@@ -74,12 +79,14 @@ namespace ServerlessFuncs
 
         [FunctionName("DeleteTodo")]
         public static IActionResult DeleteTodo(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "delete", Route = "todo/{id}")]HttpRequest req,
-            TraceWriter log, string id)
+            [HttpTrigger(AuthorizationLevel.Anonymous, "delete", Route = "todo/{id}")] HttpRequest req,
+            ILogger log, string id)
         {
+            log.LogInformation($"Deleting todo with the id {id}");
             var todo = items.FirstOrDefault(t => t.Id == id);
             if (todo == null)
             {
+                log.LogInformation($"Could not find a todo with the id {id} to delete");
                 return new NotFoundResult();
             }
             items.Remove(todo);
